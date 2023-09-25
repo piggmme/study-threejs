1ebd8b5e7469d181412e2711b820a96fda9cd098
cherry.k <cherry.k@kakaoenterprise.com>
2023-09-25 18:31:31 +0900

    :sparkles: threejs 강의 정리

---
+++ 05_utility/src/main.js
@@ -0,0 +1,5 @@
+// import example from "./example/ex01";
+// import example from "./example/ex02";
+import example from "./example/ex03";
+
+example();
---
+++ 06_transform/src/main.js
@@ -0,0 +1,6 @@
+// import example from "./example/ex01";
+// import example from "./example/ex02";
+// import example from "./example/ex03";
+import example from "./example/ex04";
+
+example();
