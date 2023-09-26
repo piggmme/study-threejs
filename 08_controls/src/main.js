4b0e7d0174a75adc5db72d087bbc8ae5be9087f0
cherry.k <cherry.k@kakaoenterprise.com>
2023-09-26 10:34:39 +0900

    :memo: camera controls 예제 추가

---
+++ 08_controls/src/main.js
@@ -0,0 +1,9 @@
+// import example from "./example/ex01";
+// import example from "./example/ex02";
+// import example from "./example/ex03";
+// import example from "./example/ex04";
+// import example from "./example/ex05";
+// import example from "./example/ex06";
+import example from "./example/ex07";
+
+example();
