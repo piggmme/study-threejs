a9d2a7cd6c9595de49f115f98b7db125b9604a6f
kheeyaa <kheeyaa888@gmail.com>
2024-01-11 20:49:46 +0900

    :sparkles: [5강] 메쉬에 대한 지오메트리 설정 방법(1) mesh의 자식으로 geometry 추가하기

--- R3F/src/MyElement3D.jsx
+++ R3F/src/MyElement3D.jsx
@@ -1,46 +1,18 @@
-import { useRef } from 'react'
 import { OrbitControls } from '@react-three/drei'
-import * as THREE from 'three'
-import { useFrame } from '@react-three/fiber'
 
 export default function MyElement3D() {
-  const refMesh = useRef()
-
-  useFrame((_, delta) => {
-    refMesh.current.rotation.y += delta
-  })
-  
   return (
     <>
       <directionalLight position={[1,1,1]} />
+      <ambientLight intensity={0.1} />
 
       <axesHelper scale={10} /> {/* world 좌표계 */}
       <OrbitControls />
 
-      <mesh 
-        ref={refMesh} 
-        position={[0,2,0]} 
-        rotation={[0, 0, THREE.MathUtils.degToRad(45)]} 
-        scale={[2,1,1]}
-      >
+      <mesh>
         <boxGeometry />
-        <meshStandardMaterial 
-          color='#e67e22' 
-          opacity={0.5} 
-          transparent={true} 
-        />
-        <axesHelper /> 
-
-        <mesh
-          scale={0.1}
-          position-y={2}
-        >
-            <sphereGeometry />
-            <meshStandardMaterial color='red' />
-            <axesHelper scale={5} /> 
-        </mesh>
+        <meshStandardMaterial color='#1abc9c'/>
       </mesh>
-        
     </>
   )
 } 
