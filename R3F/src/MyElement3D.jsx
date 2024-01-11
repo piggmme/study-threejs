ef3aa391018da1fed2b12036809a3b1aa87dcb90
kheeyaa <kheeyaa888@gmail.com>
2024-01-11 20:52:15 +0900

    ✨ [5강] 메쉬에 대한 지오메트리 설정 방법(2) drei 패키지를 사용해 지오메트리 컴포넌트 사용하기

--- R3F/src/MyElement3D.jsx
+++ R3F/src/MyElement3D.jsx
@@ -1,6 +1,7 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Box } from '@react-three/drei'
 
 export default function MyElement3D() {
+  // Drei: R3F에서 사용할 수 있는 유용한 컴포넌트들을 제공하는 라이브러리
   return (
     <>
       <directionalLight position={[1,1,1]} />
@@ -13,6 +14,11 @@ export default function MyElement3D() {
         <boxGeometry />
         <meshStandardMaterial color='#1abc9c'/>
       </mesh>
+
+      <Box position={[-1.2, 0, 0]} >
+        <meshStandardMaterial color='#8244ad'/>
+      </Box>
+
     </>
   )
 } 
