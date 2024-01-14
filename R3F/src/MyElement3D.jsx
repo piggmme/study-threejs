cf8256c646f26a4886efb1371897dd211a77589c
kheeyaa <kheeyaa@icloud.com>
2024-01-14 15:22:22 +0900

    [5강] cylinderGeometry(원기둥)

--- R3F/src/MyElement3D.jsx
+++ R3F/src/MyElement3D.jsx
@@ -7,19 +7,20 @@ export default function MyElement3D() {
   const refMesh = useRef()
   const refWireMesh = useRef()
 
-  const {radius,widthSegments,heightSegments,phiStart,phiLength,thetaStart,thetaLength} = useControls({
-    radius: {value: 1, min: 0.1, max: 5, step: 0.01},
-    widthSegments: {value: 32, min: 3, max: 256, step: 1},
-    heightSegments: {value: 32, min: 2, max: 256, step: 1},
-    phiStart: {value: 0, min: 0, max: 2*Math.PI, step: 0.01},
-    phiLength: {value: 2*Math.PI, min: 0, max: 2*Math.PI, step: 0.01},
-    thetaStart: {value: 0, min: 0, max: Math.PI, step: 0.01},
-    thetaLength: {value: Math.PI, min: 0, max: Math.PI, step: 0.01},
+  const {topRadius,bottomRadius,height,radialSegments,heightSegments,bOpen,thetaStart,thetaLength} = useControls({
+    topRadius: {value: 1, min: 0.1, max: 5, step: 0.01},
+    bottomRadius: {value: 1, min: 0.1, max: 5, step: 0.01},
+    height: {value: 1, min: 0.1, max: 5, step: 0.01},
+    radialSegments: {value: 32, min: 3, max: 256, step: 1},
+    heightSegments: {value: 1, min: 1, max: 256, step: 1},
+    bOpen: {value: false},
+    thetaStart: {value: 0, min: 0, max: 2*Math.PI, step: 0.01},
+    thetaLength: {value: 2*Math.PI, min: 0, max: 2*Math.PI, step: 0.01},
   })
 
   useEffect(()=> {
     refWireMesh.current.geometry = refMesh.current.geometry
-  }, [radius,widthSegments,heightSegments,phiStart,phiLength])
+  }, [topRadius,bottomRadius,height,radialSegments,heightSegments,bOpen,thetaStart,thetaLength])
 
   return (
     <>
@@ -30,11 +31,12 @@ export default function MyElement3D() {
       <OrbitControls />
 
       <mesh ref={refMesh}>
-        <sphereGeometry 
+        <cylinderGeometry 
           args={[
-            radius, widthSegments, heightSegments, 
-            phiStart, phiLength,
-            thetaStart, thetaLength
+            topRadius,bottomRadius,height,
+            radialSegments,heightSegments,
+            bOpen,
+            thetaStart,thetaLength
           ]}
         />
         <meshStandardMaterial color='#1abc9c'/>
