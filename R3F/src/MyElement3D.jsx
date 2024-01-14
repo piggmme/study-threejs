c8838b61c35503aa56d156e27d071caeffe21af7
kheeyaa <kheeyaa@icloud.com>
2024-01-14 16:42:06 +0900

    [6강] PBR(Physically Based Rendering) - meshPhysicalMaterial (유리질감)

--- R3F/src/MyElement3D.jsx
+++ R3F/src/MyElement3D.jsx
@@ -7,11 +7,7 @@ export default function MyElement3D() {
   const mesh1 = useRef()
   const mesh2 = useRef()
 
-  useEffect(()=>{
-    mesh2.current.material = mesh1.current.material
-  })
-
-  const {roughness,metalness} =useControls({
+  const {roughness,metalness,clearcoat,clearcoatRoughness,transmission,thickness,ior} =useControls({
     roughness: {
       value: 0,
       min: 0,
@@ -24,7 +20,42 @@ export default function MyElement3D() {
       max: 1,
       step: 0.01,
     },
+    clearcoat: {
+      value: 0,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    clearcoatRoughness: {
+      value: 0,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    transmission: {
+      value: 0,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    thickness: {
+      value: 0,
+      min: 0,
+      max: 10,
+      step: 0.01,
+    },
+    ior: {
+      value: 1.5,
+      min: 0,
+      max: 2.333,
+      step: 0.01,
+    },
   })
+
+  useEffect(()=>{
+    mesh2.current.material = mesh1.current.material
+  }, [roughness,metalness,clearcoat,clearcoatRoughness,transmission,thickness,ior])
+
 
   return (
     <>
@@ -37,11 +68,13 @@ export default function MyElement3D() {
       <axesHelper scale={10} /> {/* world 좌표계 */}
 
       <mesh ref={mesh1} position={[0.7,0,0]}>
-        <boxGeometry /> 
-        <meshStandardMaterial 
+        <torusKnotGeometry args={[0.5,0.15,256,128]} /> 
+        <meshPhysicalMaterial 
+          // meshStandardMaterial를 상속받음
+          // https://threejs.org/docs/#api/en/materials/MeshPhysicalMaterial
           visible={true}
 
-          transparent={false}
+          transparent={true}
           opacity={1}
 
           // depth buffer 
@@ -49,17 +82,27 @@ export default function MyElement3D() {
           depthWrite={true}
 
           // rendering 
-          side={THREE.FrontSide} // 앞면만
+          // side={THREE.FrontSide} // 앞면만
           // side={THREE.BackSide} // 뒷면만
-          // side={THREE.DoubleSide} // 양면
+          side={THREE.DoubleSide} // 양면
 
           wireframe={false}
 
-          color="#ff0000"
+          color="#ffffff"
           emissive="#000000" // 매쉬가 내는 빛. 기본값은 블랙
+          
           roughness={roughness} // 거칠기
           metalness={metalness} // 금속감
+          
+          clearcoat={clearcoat} // 코팅
+          clearcoatRoughness={clearcoatRoughness}
+          
           flatShading={false} // 평면 쉐이딩. 기본값은 false
+
+          // 유리 관련 속성값
+          transmission={transmission} // 투명도. 기본값은 0
+          thickness={thickness} // 유리의 두께
+          ior={ior} // 굴절률
         />
       </mesh>
 
