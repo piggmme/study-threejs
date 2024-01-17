836997999f40012c725833ee4751e538ded2b4fd
kheeyaa <kheeyaa888@gmail.com>
2024-01-17 20:41:41 +0900

    [6강] @react-three/drei - MeshReflectorMaterial

--- R3F/src/MyElement3D.jsx
+++ R3F/src/MyElement3D.jsx
@@ -1,20 +1,13 @@
-import { OrbitControls, useTexture } from '@react-three/drei'
-import { useEffect, useRef } from 'react'
+import { OrbitControls, MeshReflectorMaterial } from '@react-three/drei'
 import {useControls} from 'leva'
-import * as THREE from 'three'
+import { degToRad } from 'three/src/math/MathUtils'
 
 export default function MyElement3D() {
-  const mesh1 = useRef()
-  const mesh2 = useRef()
-
-  useEffect(()=>{
-    mesh2.current.material = mesh1.current.material
-  }, [])
-
-  // 만화같은 재질을 위한 텍스쳐
-  const texture = useTexture('./images/fiveTone.jpg')
-  texture.minFilter = THREE.NearestFilter
-  texture.magFilter = THREE.NearestFilter
+  const {rotateX, rotateY, rotateZ} = useControls({
+    rotateX: { value: -90, min: -180, max: 180, step: 45 },
+    rotateY: { value: 0, min: -180, max: 180, step: 45 },
+    rotateZ: { value: 0, min: -180, max: 180, step: 45 },
+  })
 
   return (
     <>
@@ -26,17 +19,27 @@ export default function MyElement3D() {
 
       <axesHelper scale={10} /> {/* world 좌표계 */}
 
-      <mesh ref={mesh1} position={[0.7,0,0]}>
-        <torusKnotGeometry args={[0.5,0.15,256,128]} /> 
-        <meshToonMaterial gradientMap={texture} color="cyan" /> 
-        {/* 
-          https://threejs.org/docs/?q=toon#api/en/materials/MeshToonMaterial
-          만화같은 재질
-        */}
+      <mesh position={[0,-0.6,0]} rotation={[degToRad(rotateX), degToRad(rotateY), degToRad(rotateZ)]}>
+        <planeGeometry args={[10,10]} />
+        <MeshReflectorMaterial
+          // https://github.com/pmndrs/drei?tab=readme-ov-file#meshreflectormaterial
+          // 모든 메시에 반사 및/또는 블러를 쉽게 추가할 수 있습니다. 보다 사실적인 효과를 위해 표면 거칠기를 고려합니다. 이 머티리얼은 THREE.MeshStandardMaterial에서 확장되며 모든 소품을 수용합니다.
+          blur={[300,100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={30}
+          roughness={1}
+          depthScale={0.7}
+          mixDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          color="#050505"
+          metalness={0.5}
+        />
       </mesh>
 
-      <mesh ref={mesh2} position={[-0.7,0,0]}>
-        <torusGeometry args={[0.5,0.2]} /> 
+      <mesh position={[0,0,0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="cyan" />
       </mesh>
     </>
   )
