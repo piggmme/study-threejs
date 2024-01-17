01c4830ef97fac1241edcdc58b5781e4bc379dd0
kheeyaa <kheeyaa888@gmail.com>
2024-01-17 20:20:20 +0900

    [6강] meshToonMaterial (만화같은 재질)

--- R3F/src/MyElement3D.jsx
+++ R3F/src/MyElement3D.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useTexture } from '@react-three/drei'
 import { useEffect, useRef } from 'react'
 import {useControls} from 'leva'
 import * as THREE from 'three'
@@ -11,23 +11,27 @@ export default function MyElement3D() {
     mesh2.current.material = mesh1.current.material
   }, [])
 
+  // 만화같은 재질을 위한 텍스쳐
+  const texture = useTexture('./images/fiveTone.jpg')
+  texture.minFilter = THREE.NearestFilter
+  texture.magFilter = THREE.NearestFilter
+
   return (
     <>
       <OrbitControls />
 
-      {/* <directionalLight position={[0,1,0]} />
+      <directionalLight position={[0,1,0]} />
       <directionalLight position={[1,2,8]} intensity={0.7} />
-      <ambientLight intensity={0.2} /> */}
+      <ambientLight intensity={0.2} />
 
       <axesHelper scale={10} /> {/* world 좌표계 */}
 
       <mesh ref={mesh1} position={[0.7,0,0]}>
         <torusKnotGeometry args={[0.5,0.15,256,128]} /> 
-        <meshNormalMaterial /> 
+        <meshToonMaterial gradientMap={texture} color="cyan" /> 
         {/* 
-          https://threejs.org/docs/?q=meshnormal#api/en/materials/MeshNormalMaterial
-          A material that maps the normal vectors to RGB colors.
-          번역: 법선 벡터를 RGB 색상으로 매핑하는 재질.
+          https://threejs.org/docs/?q=toon#api/en/materials/MeshToonMaterial
+          만화같은 재질
         */}
       </mesh>
 
